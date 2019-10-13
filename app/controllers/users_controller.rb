c3eabebed8160ab0a8e930595078e539b4e99b27
daichimaru <d-ichimaru@ankh-systems.co.jp>
2019-10-13 23:46:39 +0900

    recycle: MFA登録画面でmfaの確認をskipするようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: %i(new create)
+  skip_before_action :check_mfa, only: %i(new create)
   def new
     @user = User.new
   end
@@ -7,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to welcome_path, notice: 'ユーザー登録が完了しました。'
+      redirect_to welcome_path, success: 'ユーザー登録が完了しました。'
     else
       render :new
     end
